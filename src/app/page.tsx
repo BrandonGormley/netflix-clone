e4a93197607024bd14e6859a7ff46fad1824d976
Brandon Gormley <brandontgormley@gmail.com>
2023-12-20 10:26:19 -0600

    add card row component to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import CardRow from './components/CardRow';
 import Trailer from './components/Trailer';
 
 export default function Home() {
     return (
         <section>
             <Trailer />
+            <CardRow
+                title='Popular on Netflix'
+                image='/thumbnail-leaveworld.jpeg'
+            />
+            <CardRow
+                title='Continue Watching for Brandon'
+                image='/thumbnail-nowhere.jpeg'
+            />
         </section>
     );
 }
