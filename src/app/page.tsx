3f857865235f0644a01aca2d899202e8032ec56e
Brandon Gormley <brandontgormley@gmail.com>
2023-12-25 14:11:27 -0600

    add movie data to array

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,44 @@
 import CardRow from './components/CardRow';
 import Trailer from './components/Trailer';
 
+interface Movie {
+    id: number;
+    title: string;
+    image: string;
+    alt: string;
+}
+
 export default function Home() {
+    const movieData = [
+        {
+            id: 1,
+            title: 'Popular on Netflix',
+            image: '/thumbnail-leaveworld.jpeg',
+            alt: 'Thumbnail of leave the world behind',
+        },
+        {
+            id: 2,
+            title: 'Continue Watching for Brandon',
+            image: '/thumbnail-nowhere.jpeg',
+            alt: 'Thumbnail of nowhere',
+        },
+    ];
+
     return (
         <section>
             <Trailer />
             <section className='p-4 bg-black'>
-                <CardRow
-                    title='Popular on Netflix'
-                    image='/thumbnail-leaveworld.jpeg'
-                    alt='Thumbnail of leave the world behind'
-                />
-                <CardRow
-                    title='Continue Watching for Brandon'
-                    image='/thumbnail-nowhere.jpeg'
-                    alt='Thumbnail of nowhere'
-                />
+                <ul>
+                    {movieData.map((movie: Movie) => (
+                        <li key={movie.id}>
+                            <CardRow
+                                title={movie.title}
+                                image={movie.image}
+                                alt={movie.alt}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </section>
         </section>
     );
