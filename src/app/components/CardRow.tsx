48ec6a734a1add32cb0cf893ad60cd2c27ede3be
Brandon Gormley <brandontgormley@gmail.com>
2023-12-25 14:13:52 -0600

    convert html to card component

--- src/app/components/CardRow.tsx
+++ src/app/components/CardRow.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Card from './Card';
 
 interface CardRowProps {
     title: string;
@@ -11,24 +11,7 @@ export default function CardRow({ title, image, alt }: CardRowProps) {
         <div className='flex flex-col bg-black text-white lg:py-4 lg:px-16'>
             <p className='my-2 mb-4 text-xs lg:text-2xl'>{title}</p>
             <div className='flex flex-row flex-nowrap'>
-                <div className='hover:scale-110 rounded-lg cursor-pointer transition-all duration-200 mr-2'>
-                    <Image
-                        src={image}
-                        alt={alt}
-                        width={200}
-                        height={50}
-                        className='rounded-t-sm lg:w-[250px]'
-                    />
-                </div>
-                <div className='hover:scale-110 rounded-lg cursor-pointer transition-all duration-200 mr-2'>
-                    <Image
-                        src={image}
-                        alt={alt}
-                        width={200}
-                        height={50}
-                        className='rounded-t-sm lg:w-[250px]'
-                    />
-                </div>
+                <Card image={image} alt={alt} />
             </div>
         </div>
     );
